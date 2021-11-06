664bf57bb661eebf7be8374ddb9e5c2ad46e0733
Casper McTavish <johnwaiton@hotmail.co.uk>
2021-11-06 16:28:13 +0000

    Full fitting folder with all required .C files.
    Used for testing at home, not integrated into watchman but taken from it:
    https://github.com/Watchman-PMT/Wavedump_Wrapper/tree/master/Data_Analysis/Fitting

---
+++ Fitting/NotGetPeakToValley.C
@@ -0,0 +1,33 @@
+#include "NotFit_PeakAndValley.C"
+#include "TFile.h"
+#include "TString.h"
+
+using namespace std;
+
+Result* NotFit_PeakAndValley(TH1F*  fhisto);
+
+Result * NotGetPeakToValley(TString rootFileName = "Run_30_PMT_133_Loc_3_Test_S",string pathToData = "./"){
+  
+  //TString hName = "hQ_Fixed_" + rootFileName;
+  TString hName = rootFileName;
+  
+  
+  rootFileName = rootFileName + ".root";
+
+  TString rootFilePath = pathToData;
+  rootFilePath += rootFileName;
+  
+  TFile * rootFile = new TFile(rootFilePath);
+  
+  Result * result = NotFit_PeakAndValley((TH1F*)rootFile->Get(hName));
+  
+  cout << endl;
+  cout << " peak           = " << result->peak.value         << " (" << result->peak.error         << ") " << endl;
+  cout << " valley         = " << result->valley.value       << " (" << result->valley.error       << ") " << endl;
+  cout << " peak to valley = " << result->peakToValley.value << " (" << result->peakToValley.error << ") " << endl;
+  cout << " peak counts    = " << result->peakCounts.value   << " (" << result->peakCounts.error   << ") " << endl;
+  cout << " valley counts  = " << result->valleyCounts.value << " (" << result->valleyCounts.error << ") " << endl;
+
+  return result;
+}
+
---
+++ Fitting/efficiency_C.d
@@ -0,0 +1,23 @@
+
+# DO NOT DELETE
+
+./efficiency_C.so: /usr/include/TFile.h /usr/include/TH1F.h
+./efficiency_C.so: /usr/include/TF1.h /usr/include/TLine.h Poisson.C
+./efficiency_C.so: /usr/include/TMath.h /usr/include/TCanvas.h
+./efficiency_C.so: /usr/include/TGraph.h GetPeakToValley.C
+./efficiency_C.so: Fit_PeakAndValley.C /usr/include/TTree.h
+./efficiency_C.so: /usr/include/TROOT.h /usr/include/TStyle.h
+./efficiency_C.so: /usr/include/TSystem.h /usr/include/RooRealVar.h
+./efficiency_C.so: /usr/include/RooAddPdf.h /usr/include/RooGaussian.h
+./efficiency_C.so: /usr/include/RooDataHist.h /usr/include/RooFitResult.h
+./efficiency_C.so: /usr/include/RooPlot.h /usr/include/RooDerivative.h
+./efficiency_C.so: /usr/include/TSpectrum.h /usr/include/RooPolynomial.h
+./efficiency_C.so: /usr/include/RooBifurGauss.h /usr/include/RooChebychev.h
+./efficiency_C.so: /usr/include/RooBernstein.h /usr/include/RooExponential.h
+./efficiency_C.so: /usr/include/TFitResult.h /usr/include/TFitResultPtr.h
+./efficiency_C.so: /usr/include/RooAbsArg.h /usr/include/TString.h
+./efficiency_C.so: /usr/include/TLatex.h
+./efficiency_C.so: /home/casper/Wavedump_Wrapper/Common_Tools/wmStyle.C
+./efficiency_C.so: /usr/include/TPaveText.h
+./efficiency_C.so: /usr/include/RVersion.h /usr/include/RConfig.h /usr/include/TClass.h /usr/include/TDictAttributeMap.h /usr/include/TInterpreter.h /usr/include/TROOT.h /usr/include/TBuffer.h /usr/include/TMemberInspector.h /usr/include/TError.h /usr/include/RtypesImp.h /usr/include/TIsAProxy.h /usr/include/TFileMergeInfo.h /usr/include/TCollectionProxyInfo.h /usr/bin/rootcling
+efficiency_C__ROOTBUILDVERSION= 6.24/06
