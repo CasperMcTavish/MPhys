2156070817ba335cddfcca6effc3734f1daea5e7
Casper McTavish <johnwaiton@hotmail.co.uk>
2022-03-12 20:10:56 +0000

    Update histo2Ddelta.C

--- Analysis/2D_Histogram/histo2Ddelta.C
+++ Analysis/2D_Histogram/histo2Ddelta.C
@@ -46,19 +46,23 @@ void histo2Ddelta(std::string posdata = "Positions2D", std::string data = "Effic
 	TGraph2D* textbox = new TGraph2D();
 
         TH2D* h2 = new TH2D("Info",title.c_str(),21,-134,134,21,-134,134);
+        TH2D* h3 = new TH2D("Info2",title.c_str(),21,-134,134,21,-134,134);
 
 	graph->SetTitle(title.c_str());
 	// Apply file data to TGraph2D
+
+  // 1
 	int i = 0;
 	int ok = true;
 	while (ok) {
 	  // Stream values in, val1 is x, val2 is y, val3 is efficiency, val4 is uncertainty
 	  // This system can be functionalised
-	  double val1 = stream(in);
-          double val2 = stream(in);
-          double val3 = stream(inn);
-          double val4 = stream(inn);
-	  //cout << "Entry " << i << endl;
+
+    // 1
+    double val1 = stream(in);
+    double val2 = stream(in);
+    double val3 = stream(inn);
+    double val4 = stream(inn);
 	  // Checks both files for if they are at the end of file or if stream is good.
           if ((in.eof()  == true || in.good() == false) && (inn.eof()  == true || inn.good() == false)) {
 	     ok =false;
@@ -68,33 +72,66 @@ void histo2Ddelta(std::string posdata = "Positions2D", std::string data = "Effic
 	       graph->SetPoint(i, val1, val2, val3);
 
 	       h2->Fill(val1,val2,val3);
-	       // set errors
-	       //cout << "Error " << val4 << endl;
 	       h2->SetBinError(i, val4);
-               //h2->Fill(val1,val2);
-	        //cout << " val1 = " << val1 << endl;
-	       //textbox->SetPoint(i, val1, val2, val3);
 	       ++i;
 	  }
 
 	}
 	in.close();
+  inn.close();
+
+  // 2
+  // Apply file data to TGraph2D
+  int i = 0;
+  int ok = true;
+  while (ok) {
+    // Stream values in, val1 is x, val2 is y, val3 is efficiency, val4 is uncertainty
+    // This system can be functionalised
+
+    // 1
+    double val1 = stream(innn);
+    double val2 = stream(innn);
+    double val3 = stream(innnn);
+    double val4 = stream(innnn);
+    // Checks both files for if they are at the end of file or if stream is good.
+          if ((innn.eof()  == true || innn.good() == false) && (innnn.eof()  == true || innnn.good() == false)) {
+       ok =false;
+    }
+    else {
+         // Apply data to TGraph2D
+         graph->SetPoint(i, val1, val2, val3);
+
+         h3->Fill(val1,val2,val3);
+         h3->SetBinError(i, val4);
+         ++i;
+    }
+
+  }
+  innn.close();
+  innnn.close();
+
 
 	// Turn off errors (currently busted otherwise)
 	h2->Sumw2(kFALSE);
+  h3->Sumw2(kFALSE);
+
+  // Subtract
+  TH2D* h4 = new TH2D("Info3",title.c_str(),21,-134,134,21,-134,134);
+  h4->Add(h2, h3, 1.0, -1.0);
 
 	// plot graphs
 	graph->SetMarkerStyle(20);
 	graph->Draw("COLZ");
 	// remove legend
-	h2->SetStats(0);
-        h2->Draw("colz");
+	h4->SetStats(0);
+  h4->Draw("colz");
 	//textbox->SetMarkerColor(kRed);
 	//textbox->Draw("TEXT SAME");
 	// Save graph
 	string file_name = posdata;
 	file_name += "-";
 	file_name += data;
+  file_name += "_bias_";
 	string file_name_pdf = file_name;
 	file_name_pdf += ".pdf";
 	file_name += ".root";
