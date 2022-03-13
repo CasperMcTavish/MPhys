6a1321429f8aacd2c51880da216d10ed34a3332a
John Waiton <johnwaiton@hotmail.co.uk>
2022-03-13 15:39:14 +0000

    removed broken line

--- Misc Macros/plottingmacros/FullMacro.C
+++ Misc Macros/plottingmacros/FullMacro.C
@@ -9,7 +9,6 @@
 
    // Draw Dynode box
    TBox *box1 = new TBox(-12.5, -12.5, 12.5, 12.5);
-   box1->Class_Name("Dynode");
    box1->SetFillStyle(0);
    box1->SetLineColor(4);
    box1->SetLineWidth(4);
